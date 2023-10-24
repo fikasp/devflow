b345e2f43ae28a98d9e45ebfc258b8b15bd00693
fikasp <fikasp@gmail.com>
2023-10-24 02:45:58 +0200

    add auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
 	return (
 		<main className="flex-center h-screen bg-black text-white">
-			Tailwind is the best !
+			Tailwind and Clerk are the best !
+			<div>
+				<UserButton afterSignOutUrl="/" />
+			</div>
 		</main>
 	)
 }
